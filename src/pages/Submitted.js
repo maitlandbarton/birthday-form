89d8ece59fd20ec4ec835f03b5f00037beb2b40a
Mattie Barton <maitlandbarton@gmail.com>
2023-07-08 08:04:27 -0400

    make submit page responsive

--- src/pages/Submitted.js
+++ src/pages/Submitted.js
@@ -12,10 +12,10 @@ function Submitted() {
       >
         <div className="absolute inset-0 flex items-center justify-center">
           <div className="w-3/4 border lg:w-1/2 m-auto p-5 rounded-md bg-purple h-3/4 ring-4 ring-aqua shadow-xl shadow-ltPink flex flex-col items-center overflow-scroll">
-            <div className="flex flex-col items-center my-auto">
-              <h1>Thank you for your response!</h1>
+            <div className="flex flex-col items-center my-auto text-center">
+              <h1 className="text-5xl md:text-6xl lg:text-7xl text-mauve glowing-font mb-10">Thank you for your response!</h1>
               <Link to="/">
-                <button>back to party details</button>
+                <button className="bg-aqua shadow-md shadow-white hover:bg-ltPink text-xl text-purple hover:text-rose hover:shadow-md hover:shadow-rose px-4 py-2 rounded-lg">back to party details</button>
               </Link>
             </div>
           </div>
